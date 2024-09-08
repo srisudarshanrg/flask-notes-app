9f11d9acadb7b9c2d23d0bc5da68114d5bdb3fb1
R Sri Sudarshan <srisudarshanrg@gmail.com>
2024-09-08 20:40:52 +0530

    login and register functionality completed

--- notes_app/forms.py
+++ notes_app/forms.py
@@ -4,13 +4,13 @@ from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class LoginForm(FlaskForm):
     credential = StringField(label="Username or Email", validators=[DataRequired()])
-    password = StringField(label="Password", validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
     submit = SubmitField(label="Submit")
 
 class RegisterForm(FlaskForm):
-    username = StringField(label="Username", validators=[DataRequired(), Length(min=3, max=15)])
+    username = StringField(label="Username", validators=[DataRequired(), Length(min=3, max=30)])
     email = EmailField(label="Email", validators=[DataRequired(), Email()])
-    password = StringField(label="Password", validators=[DataRequired()])
-    password_confirm = StringField(label="Confirm Password", validators=[DataRequired(), EqualTo(fieldname="password")])
+    password = PasswordField(label="Password", validators=[DataRequired()])
+    password_confirm = PasswordField(label="Confirm Password", validators=[DataRequired(), EqualTo(fieldname="password")])
     submit = SubmitField(label="Submit")
 
--- notes_app/models.py
+++ notes_app/models.py
@@ -1,17 +1,17 @@
 from notes_app import db
 from notes_app import login_manager
+from flask_login import UserMixin
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(length=15), unique=True)
+    username = db.Column(db.String(length=30), unique=True)
     email = db.Column(db.String(), nullable=False, unique=True)
     password = db.Column(db.String(), nullable=False)
-    notes = db.relationship("Notes", backref="user", lazy=True)
+    notes = db.relationship("Note", backref="user", lazy=True)
 
 class Note(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
